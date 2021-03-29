e497765fdb8a70ca8fc93bc643d6b2d5858a6b3b
israelsantana01 <israelofi2@gmail.com>
2021-03-29 17:47:41 -0300

    Conclude project and hosting them in firebase

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  form: FormGroup;
 
   constructor() {}
 
+  ngOnInit() {
+    this.form = new FormGroup({
+      binary: new FormControl(null, {
+        updateOn: 'change',
+        validators: [Validators.required, Validators.pattern(/^(1|0){1,8}$/)]
+      }),
+      result: new FormControl(null, {
+        updateOn: 'change',
+        validators: []
+      })
+    });
+  }
+ 
+  changeValue() {
+    if (this.form.invalid) {
+      this.form.controls.result.patchValue('Valor inválido');
+      return;
+    }
+
+    let result: number = 0;
+    let invertedString: string[] = [];
+    let binary = this.form.value.binary;
+
+    for (let i = binary.length - 1; i >= 0; i--) {
+      invertedString.push(binary[i]);
+    }
+
+    invertedString.forEach((item, i) => {
+      result += (+item) * Math.pow(2, i);
+    })
+   
+    this.form.controls.result.patchValue(result);
+  }
 }
